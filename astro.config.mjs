959e5addcb8f83580593c9beac2eee0b5293f95c
Artur <sarnackiartur@gmail.com>
2025-12-04 16:37:21 +0100

    "feat: configure Cloudflare Pages adapter"

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import process from "node:process";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
-import node from "@astrojs/node";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,7 +17,9 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  adapter: node({
-    mode: "standalone",
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
   }),
 });
