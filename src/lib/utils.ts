79cf57027d46dd927e3c279ae3fc5de6cccfa896
Artur <sarnackiartur@gmail.com>
2026-06-14 15:36:10 +0200

    docs(utils): add JSDoc to cn and getLeagueCodeFromName

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,10 +2,20 @@ import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { LEAGUE_NAME_TO_CODE } from "../types";
 
+/**
+ * Merges Tailwind CSS classes
+ * @param inputs - Array of class values
+ * @returns Merged class string
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+/**
+ * Gets the code for a given league name
+ * @param leagueName - Name of the league
+ * @returns Code for the league
+ */
 export function getLeagueCodeFromName(leagueName: string): string {
   return LEAGUE_NAME_TO_CODE[leagueName];
 }
